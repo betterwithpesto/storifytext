b8ee97e65eedf6e54b911500cefd2b048f7790fd
betterwithpesto <pat@prost.io>
2013-12-10 02:10:20 -0600

    storify twitter and storify text only

---
+++ main.rb
@@ -0,0 +1,24 @@
+require 'rubygems'  
+require 'sinatra'
+require 'json'
+require 'pp'
+require 'open-uri'
+require 'nokogiri'
+
+
+
+get '/' do
+	erb :storify
+end
+
+post '/' do
+
+  url = params[:storify]
+  if !url.include? ".html"
+  	url+=".html"
+  end
+  @data = Nokogiri::HTML(open(url))
+  
+  
+  erb :result
+end
